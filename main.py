ee3b9d7969aa4de064c5481333a53841278b31ed
nikola <nikola@vlending.co.kr>
2022-10-14 00:06:09 +0900

    model VARCHAR 문자열 정의, 기본 모델 추가

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from sql_app import models
+from sql_app.database import SessionLocal, engine
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+models.Base.metadata.create_all(bind=engine)
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/home/{item_id}", response_class=HTMLResponse)
-async def render_main(request: Request, item_id: int):
-    return templates.TemplateResponse('index.html', context={'request': request, "item_id": item_id})
-
+# Dependency
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,10 +1,36 @@
 from pydantic import BaseModel
 
 
-class Item(BaseModel):
-    id: int
+class ItemBase(BaseModel):
     title: str
     description: str | None = None
+    img_url: str | None = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: list[Item] = []
 
     class Config:
         orm_mode = True
