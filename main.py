6f2fe1521acbc5b84446b8ee7225e375b341dce9
nikola <nikola@vlending.co.kr>
2022-11-08 14:40:29 +0900

    router 모듈 연동, 코드 정리

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
+from routers import router_main
 from sql_app import models
-from sql_app.database import SessionLocal, engine
+from sql_app.database import engine
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 models.Base.metadata.create_all(bind=engine)
 
-
-# Dependency
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+app.include_router(router_main.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- routers/router_main.py
+++ routers/router_main.py
@@ -1,10 +1,13 @@
-from fastapi import Request, Depends, HTTPException
+from fastapi import Request, Depends, HTTPException, APIRouter
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from sql_app import crud, schemas
 from sql_app.database import SessionLocal
-from main import app, templates
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter()
 
 
 # Dependency
@@ -16,17 +19,17 @@ def get_db():
         db.close()
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@router.get("/", response_class=HTMLResponse, tags=["main_page"])
+async def read_root(request: Request):
+    return templates.TemplateResponse('main_page.html', context={'request': request})
 
 
-@app.get("/home/{item_id}", response_class=HTMLResponse)
+@router.get("/home/{item_id}", response_class=HTMLResponse, tags=["items"])
 async def render_main(request: Request, item_id: int):
     return templates.TemplateResponse('index.html', context={'request': request, "item_id": item_id})
 
 
-@app.post("/users/", response_model=schemas.User)
+@router.post("/users/", response_model=schemas.User, tags=["users"])
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
@@ -34,13 +37,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/", response_model=list[schemas.User])
+@router.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
+@router.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
@@ -48,14 +51,14 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+@router.post("/users/{user_id}/items/", response_model=schemas.Item)
 def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=list[schemas.Item])
+@router.get("/items/", response_model=list[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
